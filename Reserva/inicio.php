8ed990dfe17edfcee13a827b1b327392e6ef63b6
Alexjairo <jairomorocho5@gmail.com>
2019-02-13 03:52:27 -0500

    Gitacora saved 1 changed files

--- Reserva/inicio.php
+++ Reserva/inicio.php
@@ -104,7 +104,6 @@
 					</p>
 					<p class="pull-right">
 						<ul class="gtco-social-icons pull-right">
-							<li><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ"><i class="icon-twitter"></i></a></li>
 							<li><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ"><i class="icon-linkedin"></i></a></li>
 							<li><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ"><i class="icon-dribbble"></i></a></li>
 						</ul>
