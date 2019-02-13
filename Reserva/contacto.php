d2e02f1d2fb52292482d4876ef23a1b57fe37f13
Alexjairo <jairomorocho5@gmail.com>
2019-02-13 04:45:21 -0500

    Gitacora saved 1 changed files

--- Reserva/contacto.php
+++ Reserva/contacto.php
@@ -110,7 +110,7 @@
 						<h3>Contacto Informacion</h3>
 						<ul>
 							<li class="address">Ecuador, <br>Quito</li>
-							<li class="phone"><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ">+57 018008956</a></li>
+							<li class="phone">+593 5131501</a></li>
 							<li class="email"><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ">ingeniero292@gmail.com</a></li>
 							<li class="url"><a href="https://www.youtube.com/channel/UC87BrnQy7Y6iipGvQ6sAJfQ">Chiphysi Programacion</a></li>
 						</ul>
