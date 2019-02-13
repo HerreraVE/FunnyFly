737a524253a3bda789944ec21bc4931f91a830c9
Alexjairo <jairomorocho5@gmail.com>
2019-02-13 04:09:09 -0500

    Gitacora saved 1 changed files

--- Reserva/inicio.php
+++ Reserva/inicio.php
@@ -77,13 +77,13 @@
 					</div>
 <center>
 	<a href="reserva.php" >
-					<button class="btn btn-info" color="black"><font size="4">RESERVAS</font><br><br><img src="images/reserva.png" width="150"></button>
+					<button class="btn btn-info" ><font size="4"></font><br><br><img src="images/reserva.png" width="150"></button>
 	</a>
 	<a href="precios.php">				
-					<button class="btn btn-info"><font size="4">PRECIOS</font><br><br><img src="images/precio.png" width="150"></button>
+					<button class="btn btn-info"><font size="4"></font><br><br><img src="images/precio.png" width="150"></button>
 	</a>
 	<a href="contacto.php">				
-					<button class="btn btn-info"><font size="4">CONTACTO</font><br><br><img src="images/contacto.png" width="150"></button>
+					<button class="btn btn-info"><font size="4"></font><br><br><img src="images/contacto.png" width="150"></button>
 	</a>				
 </center>							
 					
