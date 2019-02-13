d9b15299dd540336f6d7f8f1d1f5ea18773a34b1
Alexjairo <jairomorocho5@gmail.com>
2019-02-13 03:43:29 -0500

    Gitacora saved 1 changed files

--- Reserva/conexion3.php
+++ Reserva/conexion3.php
@@ -12,7 +12,7 @@
 
 
 
-		$mysqli = new MySQLi("localhost", "root","", "sugerencia");
+		$mysqli = new MySQLi("localhost", "root","pokemonGO99", "sugerencia");
 		if ($mysqli -> connect_errno) {
 			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
 				. ") " . $mysqli -> mysqli_connect_error());
